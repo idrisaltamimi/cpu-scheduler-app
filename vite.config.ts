8e30ce2628450ff74de5dc7613a0c8e8682c0016
Idris Al Tamimi <idris.al.tamimi.1@gmail.com>
2025-12-23 21:52:31 +0400

    added base

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'node',
+    environment: "node"
   },
+  base: "/cpu-scheduler-app/"
 })
